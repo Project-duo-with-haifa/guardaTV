26830accb74d0971e7f4d93a3c882a818a37d757
gharradhaifa <gharradhaifa6@gmail.com>
2022-12-08 19:50:12 +0100

    update

--- signup.js
+++ signup.js
@@ -1,25 +1,21 @@
+ var getUser = [];
+function newUser(){
 
-function NewUser(){
 	var user={}
 	
+
 user.name=document.forms["myForm"]["full_name"].value;
 user.email=document.forms["myForm"]["email"].value;
 user.password=document.forms["myForm"]["psw"].value;
-window.localStorage.setItem('user', JSON.stringify(user));
-window.localStorage.getItem('user');
 
- var getUser = [];
-   var get =  JSON.parse(localStorage.getItem('user'));
-   getUser = [get];
-   getUser.push(user);
-console.log(JSON.stringify(getUser));
-   console.log(getUser);
 
-/*var getUser=[]
-getUser.push(JSON.parse(window.localStorage.getItem('user'));
-	localStorage.setItem('user', JSON.stringify(getUser));
 
-   */
+   var get =  JSON.parse(localStorage.getItem('getUser'));
+  get.push(user);
+  console .log(get
+window.localStorage.setItem('getUser', JSON.stringify(get));
+
+
 
 alert(getUser)
 
