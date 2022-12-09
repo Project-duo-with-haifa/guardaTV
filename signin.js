61524ad4a950cf4eca0cb1866f564d48e840488c
gharradhaifa <gharradhaifa6@gmail.com>
2022-12-09 10:32:34 +0100

    last update

--- signin.js
+++ signin.js
@@ -1,8 +1,28 @@
+var users = []
+var MakeUser= function(username,email,pw){
+	var user ={}
+	user.name=username;
+	user.email = email; 
+	user.password = pw 
+	users.push(user)
+	return user 
+}
+
+
+var haifa1 = MakeUser("haifa","haifa@rbk.tn","ttttttt")
+var Haifa = MakeUser("haifa","haifa@gmail.com","Azertysaif9")
+var Ala = MakeUser("ala","ala@gmail.com","vivobn")
+
 
 function login(){
 
 email=document.forms["myForm"]["email"].value;
 password=document.forms["myForm"]["psw"].value;
-if(email==="admin@gmail.com"){
-document.getElementById("signinbtn").innerHTML="<a href='welcome.html'>Sign In</a>"
-}}
+
+		if(email==="haifa@rbk.tn"){
+			$("#signinbtn").html("<a href='user.html'>Sign In</a>")
+		}
+		else if (email==="haifa@gmail.com"){$("#signinbtn").html("<a href='user.html'>Sign In</a>")}
+
+			else if(email==="ala@gmail.com"){$("#signinbtn").html("<a href='user.html'>Sign In</a>")}
+			else {alert("Sign up first please!")} }
--- signup.js
+++ signup.js
@@ -1,22 +1,29 @@
+ /*
+
  var getUser = [];
-function newUser(){
+ function newUser(){
+ var user={}
+ user.name=document.forms["myForm"]["full_name"].value;
+ user.email=document.forms["myForm"]["email"].value;
+ user.password=document.forms["myForm"]["psw"].value;
 
-	var user={}
-	
+ var get =  JSON.parse(localStorage.getItem('getUser'));
+ get.push(user);
+ console .log(get)
+ window.localStorage.setItem('getUser', JSON.stringify(get));
+ alert(getUser)}*/
 
-user.name=document.forms["myForm"]["full_name"].value;
-user.email=document.forms["myForm"]["email"].value;
-user.password=document.forms["myForm"]["psw"].value;
-
-
-
-   var get =  JSON.parse(localStorage.getItem('getUser'));
-  get.push(user);
-  console .log(get
-window.localStorage.setItem('getUser', JSON.stringify(get));
-
-
-
-alert(getUser)
-
+var users = []
+var MakeUser= function(username,email,pw){
+   var user ={}
+   user.name=username;
+   user.email = email; 
+   user.password = pw 
+   users.push(user)
+   return user 
 }
+
+
+
+var Hadil = MakeUser("Hadil","alaaloulou@rbk.coffe","taketenguys")
+var Haifa = MakeUser("Haifa","wahbi@wawa.com","Azertysaif9")
